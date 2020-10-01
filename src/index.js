69181b490d57837338bdc5ce038e4e83ff306e32
superbobr <leonseoprofi@gmail.com>
2020-10-01 16:03:19 +0300

    the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+      return 0;
+  } else {
+      let min = array[0];
+      for (let number of array.slice(1, )) {
+        if (number < min) {
+            min = number;
+        }
+      }
+      return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let max = array[0];
+        for (let number of array.slice(1, )) {
+          if (number > max) {
+              max = number;
+          }
+        }
+        return max;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        for (let number of array) {
+            sum += number;
+        }
+        return sum / array.length;
+    }
+};
